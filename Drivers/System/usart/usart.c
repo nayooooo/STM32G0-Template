96e613b94d746c5d87bf5aafd0bb33ef7d16852e
nayooooo <YEWANhup@outlook.com>
2023-01-27 05:06:30 +0800

    整理了一下usart的代码罗列结构

--- Drivers/System/usart/usart.c
+++ Drivers/System/usart/usart.c
@@ -33,25 +33,53 @@
  
 
 //////////////////////////////////////////////////////////////////
-//加入以下代码,支持printf函数,而不需要选择use MicroLIB	  
+//加入以下代码,支持printf函数,而不需要选择use MicroLIB
 #if 1
-//#pragma import(__use_no_semihosting)            
-__asm (".global __use_no_semihosting\n\t");
 
-FILE __stdout;       
-//定义_sys_exit()以避免使用半主机模式    
-void _sys_exit(int x) 
-{ 
-	x = x; 
-} 
-//重定义fputc函数 
-int fputc(int ch, FILE *f)
-{      
-	while((USART1->ISR&0X40)==0){}//循环发送,直到发送完毕   
-    USART1->TDR = (uint8_t) ch;      
+#if (__ARMCC_VERSION >= 6010050)				/* 使用AC6编译器 */
+__asm (".global __use_no_semihosting\n\t");		/* 声明不使用半主机模式 */
+__asm (".global __ARM_use_no_argv \n\t");		/* AC6不需要声明main函数为无参数格式，否则部分例程可能出现半主机模式 */
+#else											/* 使用AC5编译器 */
+//#pragma import(__use_no_semihosting)
+
+struct __FILE
+{
+	int handle;
+}
+#endif
+
+// 不使用半主机模式，至少需要重定义__ttywrch\_sys_exit\_sys_command_string函数，以同时兼容AC6和AC5模式
+int _ttywrch(int ch)
+{
+	ch = ch;
 	return ch;
 }
-#endif 
+
+// 定义_sys_exit()以避免使用半主机模式
+void _sys_exit(int x)
+{
+	x = x;
+}
+
+char *_sys_command_string(char *cmd, int len)
+{
+	return NULL;
+}
+
+// FILE在stdio.h里面定义
+FILE __stdout;
+#endif  /* 代码法 */
+
+#if 1
+// 重定义fputc函数
+int fputc(int ch, FILE *f)
+{
+	// 循环发送,直到发送完毕
+	while((USART1->ISR&0X40)==0){}
+    USART1->TDR = (uint8_t) ch;
+	return ch;
+}
+#endif /* 重定义fputc */
 
 
 #if EN_USART1_RX   //如果使能了接收
